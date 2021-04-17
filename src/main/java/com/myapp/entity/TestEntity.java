7d54831a4545cd34a84bc62d6024fc73d8252d5e
PabloRS <prmzs89@hotmail.com>
2021-04-17 17:18:57 -0500

    Primer commit del proyecto my-app

---
+++ src/main/java/com/myapp/entity/TestEntity.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.myapp.entity;
+
+/**
+ * @author pablo
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para compilación con maven.
+	 */
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
